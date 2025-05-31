9670d9ce6834ca2f20726e58d971937daeb6f220
Frankish0014 <f.ishimwe@alustudent.com>
2025-05-31 12:16:43 +0200

    I love C

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,14 +3,14 @@
 // Get the first argument from the command line
 const x = process.argv[2];
 
-// Check if the argument is a valid positive integer
+// Convert the argument to an integer
 const num = parseInt(x, 10);
 
 // If num is NaN or less than 1, print the error message
 if (isNaN(num) || num < 1) {
   console.log("Missing number of occurrences");
 } else {
-// Loop to print "C is fun" x times
+  // Loop to print "C is fun" num times
   for (let i = 0; i < num; i++) {
     console.log("C is fun");
   }
