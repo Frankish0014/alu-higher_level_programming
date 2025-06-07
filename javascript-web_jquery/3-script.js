2eb1fc07fb3af6e8299032a912476e524c170c8b
Frankish0014 <f.ishimwe@alustudent.com>
2025-06-08 00:19:08 +0200

    jQuery tasks

--- javascript-web_jquery/3-script.js
+++ javascript-web_jquery/3-script.js
@@ -1,3 +1,6 @@
-$('red_header').click(function () {
-	$('header').addClass('red');
+$(document).ready(function() {
+	$('#red_header').click(function () {
+		$('header').addClass('red');
+	});
 });
+
