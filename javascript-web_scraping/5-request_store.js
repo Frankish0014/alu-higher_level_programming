47972713411f259083c9e0711cb15df95bc81cfe
Frankish0014 <f.ishimwe@alustudent.com>
2025-06-07 17:30:04 +0200

    indentation

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,18 +1,14 @@
 #!/usr/bin/node
-const request = require('request');
+
 const fs = require('fs');
+const request = require('request');
 
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else {
-        fs.writeFile(filePath, body, 'utf8', (err) => {
-            if (err) {
-                console.error(err);
-            }
-        });
-    }
-});
+request(url)
+  .pipe(fs.createWriteStream(filePath))
+  .on('finish', () => {
+    console.log(`Downloaded and saved ${fs.statSync(filePath).size} bytes to ${filePath}`);
+  });
+
