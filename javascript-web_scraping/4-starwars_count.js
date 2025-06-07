e35ae41be402d0fe61d1d9236caad09f43ad7ec0
Frankish0014 <f.ishimwe@alustudent.com>
2025-06-07 17:47:31 +0200

    indentation

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,13 @@
-#!/usr/bin/node
-
 const request = require('request');
 
-const url = 'https://swapi-api.alx-tools.com/api/films'; // Update this URL if necessary
+const url = process.argv[2];
 
-const options = {
-  url: url,
-  rejectUnauthorized: false // Bypass SSL verification (use with caution)
-};
-
-request(options, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const films = JSON.parse(body);
-    const filmCount = films.count || 0; // Adjust based on the expected output structure
-    console.log(filmCount); // Output the number of films
-  }
+request(url, (error, response, body) => {
+    if (error) {
+        console.error(error);
+    } else {
+        const movies = JSON.parse(body).results;
+        const count = movies.filter(movie => movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')).length;
+        console.log(count);
+    }
 });
