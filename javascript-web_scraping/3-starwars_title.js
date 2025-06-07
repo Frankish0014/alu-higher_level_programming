2ecf99fbdf2c562a0fe9299a1b2a86831719eda6
Frankish0014 <f.ishimwe@alustudent.com>
2025-06-07 17:51:40 +0200

    indentation

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,13 @@
-#!/usr/bin/node
-
 const request = require('request');
 
-const url = process.argv[2];
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url, (error, response) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    console.log('Code:', response && response.statusCode ? response.statusCode : 'No response');
-  }
+request(url, (error, response, body) => {
+    if (error) {
+        console.error(error);
+    } else {
+        const movie = JSON.parse(body);
+        console.log(movie.title);
+    }
 });
